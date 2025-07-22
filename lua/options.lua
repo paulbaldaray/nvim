14372c1d73e7be6823cbc736b02245c74e41778b
Paul Baldaray <paul.baldaray@snowflake.com>
2025-07-22 09:34:11 -0700

    clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -9,6 +9,19 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = '80,120'
 
--- Misc
+-- Clipboard
 vim.opt.clipboard = 'unnamedplus'
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  }
+}
+
+-- Misc
 vim.opt.timeoutlen=300
