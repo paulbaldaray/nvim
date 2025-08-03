f024184b464cc4947416b1c4aca6f9663988ca63
Paul Baldaray <paul.baldaray@snowflake.com>
2025-08-03 13:18:39 -0700

    cmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,8 @@
 
 -- Basics
 vim.g.mapleader = " "
-vim.keymap.set("i", "fj", "<Esc><Esc>")
+vim.keymap.set("i", "fj", "<Esc>")
+vim.keymap.set("c", "fj", "<Esc>")
 
 -- Scroll
 vim.keymap.set("n", "<C-e>", "3<C-e>")
@@ -11,3 +12,9 @@ vim.keymap.set("n", "<C-y>", "3<C-y>")
 -- Quit
 vim.keymap.set("n", "<leader>x", ":q<CR>")
 vim.keymap.set("n", "<leader>xx", ":q!<CR>")
+
+-- Window
+vim.keymap.set("n", "<C-Down>", "<cmd>resize +2<cr>")
+vim.keymap.set("n", "<C-Up>", "<cmd>resize -2<cr>")
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize -2<cr>")
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize +2<cr>")
