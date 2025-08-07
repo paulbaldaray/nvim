19afc24fa3a4867c5233175bd470befbba3d15ef
Paul Baldaray <paul.baldaray@snowflake.com>
2025-08-07 15:21:23 -0700

    bye bye blink

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "williamboman/mason-lspconfig.nvim", enabled = false },
   { "hrsh7th/nvim-cmp", enabled = false },
+  { "saghen/blink.cmp", enabled = false },
 }
