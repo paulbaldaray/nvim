7132a031950aa85770dd58c5594cbb764430ed46
Paul Baldaray <paul.baldaray@snowflake.com>
2025-08-07 15:10:40 -0700

    disable cmp

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "mfussenegger/nvim-lint", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "hrsh7th/nvim-cmp", enabled = false },
 }
