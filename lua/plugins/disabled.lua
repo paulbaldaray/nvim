1430c67ca44754885e912dc6621699b2983186e6
Paul Baldaray <paul.baldaray@snowflake.com>
2025-08-15 11:00:38 -0700

    remove bloat

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,4 +4,6 @@ return {
   { "williamboman/mason-lspconfig.nvim", enabled = false },
   { "hrsh7th/nvim-cmp", enabled = false },
   { "saghen/blink.cmp", enabled = false },
+  { "mason-org/mason.nvim", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
 }
