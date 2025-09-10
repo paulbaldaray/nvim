bb20776cfcb952caafa78d7c72bc8198b67cceb5
Paul Baldaray <paul.baldaray@snowflake.com>
2025-09-09 23:44:38 -0700

    claudecode plugin

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,29 @@
+local toggle_key = "<leader>cc"
+return {
+	{
+		"coder/claudecode.nvim",
+		dependencies = { "folke/snacks.nvim" },
+		keys = {
+			{ toggle_key, "<cmd>ClaudeCodeFocus<cr>", desc = "Claude Code", mode = { "n", "x" } },
+			{ toggle_key, "<cmd>ClaudeCodeSend<cr>", desc = "Claude Send Select", mode = "v" },
+		},
+		opts = {
+			terminal = {
+				---@module "snacks"
+				---@type snacks.win.Config|{}
+				snacks_win_opts = {
+					keys = {
+						claude_hide = {
+							toggle_key,
+							function(self)
+								self:hide()
+							end,
+							mode = "t",
+							desc = "Hide",
+						},
+					},
+				},
+			},
+		},
+	},
+}
