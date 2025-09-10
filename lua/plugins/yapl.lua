f404219dbee9fb2d52b12d928634ca68a2cae57a
Paul Baldaray <paul.baldaray@snowflake.com>
2025-09-10 05:06:56 -0700

    YAML Agentic Programming Language Support

---
+++ lua/plugins/yapl.lua
@@ -0,0 +1,49 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "yaml" })
+		end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = function()
+			-- Set up filetype detection for .yapl files
+			vim.filetype.add({
+				extension = {
+					yapl = "yaml",
+				},
+			})
+
+			-- Set up autocmds for YAPL files
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				pattern = "*.yapl",
+				callback = function()
+					vim.bo.filetype = "yaml"
+					vim.bo.commentstring = "# %s"
+					vim.bo.tabstop = 2
+					vim.bo.shiftwidth = 2
+					vim.bo.expandtab = true
+
+					-- Use matchadd for highlighting instead of syntax
+					vim.defer_fn(function()
+						-- Define highlight groups
+						vim.api.nvim_set_hl(0, "YaplKeywordType", { fg = "#cba6f7", bold = true })
+						vim.api.nvim_set_hl(0, "YaplKeywordLet", { fg = "#cba6f7", bold = true })
+						vim.api.nvim_set_hl(0, "YaplKeywordInclude", { fg = "#cba6f7", bold = true })
+						vim.api.nvim_set_hl(0, "YaplKeywordFor", { fg = "#cba6f7", bold = true })
+						vim.api.nvim_set_hl(0, "YaplVariable", { fg = "#fab387" })
+
+						-- Add matches
+						vim.fn.matchadd("YaplKeywordType", "^\\s*\\zsyapl\\ze\\s*:")
+						vim.fn.matchadd("YaplKeywordLet", "^\\s*\\zslet\\ze\\s*:")
+						vim.fn.matchadd("YaplKeywordInclude", "^\\s*\\zsinclude\\ze\\s*:")
+						vim.fn.matchadd("YaplKeywordFor", "^\\s*-\\?\\s*\\zsfor\\ze\\s\\+\\w\\+\\s\\+in\\s")
+						vim.fn.matchadd("YaplVariable", "<[a-zA-Z_][a-zA-Z0-9_]*>")
+					end, 200)
+				end,
+			})
+		end,
+	},
+}
