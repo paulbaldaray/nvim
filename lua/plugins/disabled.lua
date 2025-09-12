7586936c45079d08129a95335769c01e002d33ee
Paul Baldaray <paul.baldaray@snowflake.com>
2025-09-12 15:29:18 -0700

    disable lsp

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,9 @@ return {
 	{ "echasnovski/mini.pairs", enabled = false },
 	{ "hrsh7th/nvim-cmp", enabled = false },
 	{ "saghen/blink.cmp", enabled = false },
+	-- Disable LSP and mason
+	{ "neovim/nvim-lspconfig", enabled = false },
+	{ "williamboman/mason.nvim", enabled = false },
+	{ "williamboman/mason-lspconfig.nvim", enabled = false },
+	{ "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
 }
