98d983a730dc0e1d36d6d096e5e7c86500948d19
Paul Baldaray <paul.baldaray@snowflake.com>
2025-09-12 15:28:25 -0700

    1 scratch buffer + toggle scratch buffer with leader'

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,16 +1,23 @@
+local scratch_file = vim.fn.stdpath("data") .. "/scratch.md"
+
 return {
 	"folke/snacks.nvim",
 	lazy = false,
 	opts = {
 		scratch = {
 			ft = "markdown",
+			file = scratch_file,
 		},
 	},
 	keys = {
 		{
 			"<leader>'",
 			function()
-				Snacks.scratch()
+				if vim.fn.expand("%:p") == scratch_file then
+					vim.cmd("close")
+				else
+					Snacks.scratch()
+				end
 			end,
 			desc = "Toggle Scratch Buffer",
 		},
